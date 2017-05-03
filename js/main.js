5849d49bf08b53da16241080ac8315d6906d3851
Szymon Ulewicz <szymon.ulewicz@gmail.com>
2017-05-03 21:45:38 +0200

    Opening main window.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
 "use strict";
 
-var path = require('path');
-var bridge = require('./js/node_bridge.js')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
-var gui = require('nw.gui');
-var menuBar = new gui.Menu({
-	type: 'menubar'
-});
-if (menuBar.createMacBuiltin) {
-	menuBar.createMacBuiltin('Universal Editor', {
-		hideEdit: false,
-	});
-	gui.Window.get().menu = menuBar;
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 1024, height: 768});
+  Menu.setApplicationMenu(null);
+  
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, '../index.html'),
+    protocol: 'file:',
+    slashes: true,
+    show: false
+  }));
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 }
 
-window.onload = function () {
-	var editorNode = document.getElementById('editor');
-	var errorNode = document.getElementById('error');
-	if (me.Metadata) {
-		editorNode.style.display = 'block';
-		var editor = new me.Editor();
-		var controller = new me.EditorController(editor);
-	} else {
-		errorNode.innerHTML = 'No <b>game_metadata.js</b> file found in ' + path.resolve(bridge.__dirname, '../../') + '/.<br>You can copy one from ' + path.resolve(bridge.__dirname, '../sample/') + '/.';
-		errorNode.style.display = 'block';
-	}
-};
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
